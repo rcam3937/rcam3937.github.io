3faf457eb345f0c2d3c1dccb994e4343b1b4f567
Jeffords <jcam3937@gmail.com>
2025-07-27 21:03:54 -0500

    update scoring logic

--- calc.js
+++ calc.js
@@ -1,8 +1,8 @@
-//auto
+//Auto
 lowerLedgeValueAuto = 2;
 upperLedgeValueAuto = 3;
 summitValueAuto = 4;
-//telop
+//Telop
 lowerLedgeValueTelop = 1;
 upperLedgeValueTelop = 2;
 summitValueTelop = 3;
@@ -10,16 +10,39 @@ summitValueTelop = 3;
 penaltiesValue = -3
 
 function calcLedgePoints() {
+    const llTotal = 0;
     const llAuto = (document.getElementById("ll-auto").value || 0) * lowerLedgeValueAuto;
     const llTelop = (document.getElementById("ll-telop").value || 0) * lowerLedgeValueTelop;
+    const llBonus = document.getElementById("ll-true").checked;
+    console.log(llBonus);
+    if(llBonus) {
+        llTotal = (llAuto + llTelop) * 2;
+    } else {
+        llTotal = llAuto + llTelop;
+    }
 
+    const ulTotal = 0;
     const ulAuto = (document.getElementById("ul-auto").value || 0) * upperLedgeValueAuto;
     const ulTelop = (document.getElementById("ul-telop").value || 0) * upperLedgeValueTelop;
+    const ulBonus = document.getElementById("ul-true").checked;
+    console.log(ulBonus);
+    if(ulBonus) {
+        ulTotal = (ulAuto + ulTelop) * 2;
+    } else {
+        ulTotal = ulAuto + ulTelop;
+    }
 
+    const sTotal = 0;
     const sAuto = (document.getElementById("s-auto").value || 0) * summitValueAuto;
     const sTelop = (document.getElementById("s-telop").value || 0) * summitValueTelop;
+    const sBonus = document.getElementById("s-true");
+    if(sBonus) {
+        sTotal = (slAuto + sTelop) * 2;
+    } else {
+        sTotal = sAuto + sTelop;
+    }
 
-    score = llAuto+llTelop+ulAuto+ulTelop+sAuto+sTelop;
+    const score = llTotal + ulTotal + sTotal;
 
     console.log(score);
 
@@ -30,7 +53,7 @@ function calcLedgePoints() {
 }
 
 function calcTraversePoints() {
-    
+
 }
 
 function calcPenalties() {
@@ -41,4 +64,6 @@ function calcPenalties() {
     return penalties
 }
 
-
+function calcTotalPoints() {
+    return calcLedgePoints() + calcTraversePoints() + calcPenalties();
+}
